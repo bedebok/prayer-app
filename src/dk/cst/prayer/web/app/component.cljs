322e22007466cd1c76ac6a8f79c85df7a0eb9470
Simon Gray <simongray@gmail.com>
2024-11-20 14:06:00 +0100

    paginated display of texts

--- src/dk/cst/prayer/web/app/component.cljs
+++ src/dk/cst/prayer/web/app/component.cljs
@@ -1,6 +1,8 @@
 (ns dk.cst.prayer.web.app.component
   "Functions returning Replicant-style Hiccup."
   (:require [dk.cst.prayer.web.app.event :as event]
+            [dk.cst.hiccup-tools.hiccup :as h]
+            [dk.cst.hiccup-tools.elem :as e]
             [dk.cst.prayer.web.shared :as page]))
 
 (defn nav
@@ -20,11 +22,34 @@
    [:details [:summary "state"]
     [:pre (with-out-str (cljs.pprint/pprint state))]]])
 
+(defn node->pages
+  [node]
+  (as-> (h/get node :tei-body) $
+        (h/split :tei-pb $ :retain :between)
+        (e/children $)
+        (partition-by #(= :tei-pb (first %)) $)
+        (partition 2 $)))
+
+(defn text-display
+  [node]
+  [:section.pages
+   (for [[pb content] (node->pages node)]
+     (let [data-n (-> pb first second :data-n)]
+       (into [:article.page [:header data-n] content])))])
+
+(defn entity-display
+  [{:keys [bedebok/type file/node] :as entity}]
+  (condp = type
+    "text" (text-display node)
+
+    ;; TODO: for dev usage, remove eventually
+    [:pre (str entity)]))
+
 (defn content
   [{:keys [location] :as state}]
   (let [{:keys [name params]} location]
     (condp = name
-      ::page/entity (get-in state [:entities (:id params) :file/node]))))
+      ::page/entity (entity-display (get-in state [:entities (:id params)])))))
 
 (defn page
   [state]
